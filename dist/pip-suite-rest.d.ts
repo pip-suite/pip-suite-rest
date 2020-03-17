570f53669b9a3e24d970ed9eee1aad6c2c8c92ce
Aleksandr Kpsheverov <iworb.ne@gmail.com>
2020-03-17 15:25:01 +0300

    'themes for entry'

--- dist/pip-suite-rest.d.ts
+++ dist/pip-suite-rest.d.ts
@@ -38,6 +38,27 @@ export interface IAuthStateProvider extends ng.IServiceProvider {
 
 
 
+export interface IDataModelService {
+    execute(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
+    create(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
+    update(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
+    delete(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
+    read(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
+    readOne(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
+    page(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
+    uploadFiles(params: any, successCallback?: () => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
+    save(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
+    remove(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
+    query(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
+    get(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
+    readPage(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
+    queryPage(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
+}
+export interface IDataModelProvider extends ng.IServiceProvider {
+}
+
+
+
 export class CacheParams {
     force?: boolean;
     resource?: string;
@@ -66,27 +87,6 @@ export interface IDataCacheService {
     decorateRemoveCallback(resource: string, params?: CacheParams, successCallback?: (data: any) => void): Function;
 }
 export interface IDataCacheProvider extends ng.IServiceProvider {
-}
-
-
-
-export interface IDataModelService {
-    execute(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
-    create(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
-    update(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
-    delete(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
-    read(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
-    readOne(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
-    page(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
-    uploadFiles(params: any, successCallback?: () => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
-    save(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
-    remove(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
-    query(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
-    get(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
-    readPage(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
-    queryPage(params: any, successCallback?: (result: any) => void, errorCallback?: (error: any) => void): ng.IPromise<{}>;
-}
-export interface IDataModelProvider extends ng.IServiceProvider {
 }
 
 
